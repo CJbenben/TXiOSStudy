be508a0a221b575269029d54512fa1b6a8483962
ChenJie <chenjie@ChenJiedeMac-mini.local>
2017-10-25 20:49:21 +0800

    我在version。1

--- CJHttpRequest/AppDelegate.m
+++ CJHttpRequest/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // 我在 version1.1 分支测试代码
     return YES;
 }
 

3070f280a604399406d21cde04b548fbf9673257
chenxiaojie <chenjie003@qq.com>
2022-03-08 14:50:39 +0800

    modify .gitignore

--- 08-runtime/RuntimeDemo/RuntimeDemo/ViewController.m
+++ 08-runtime/RuntimeDemo/RuntimeDemo/ViewController.m
@@ -17,26 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    
-    TXStudent *student = [[TXStudent alloc] init];
-    
-    BOOL a = [student isMemberOfClass:[TXStudent class]];   // 1
-    BOOL b = [student isMemberOfClass:[TXPerson class]];    // 0
-    BOOL c = [student isKindOfClass:[TXStudent class]];     // 1
-    BOOL d = [student isKindOfClass:[TXPerson class]];      // 1
-    
-    TXPerson *person = [[TXPerson alloc] init];
-    BOOL e = [student respondsToSelector:@selector(test)];
-    BOOL f = [person respondsToSelector:@selector(test2)];
-    NSObject *obj;
-    NSLog(@"1111111");
-    
-    
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    [[NSNotificationCenter defaultCenter] addObserverForName:@"notificationTest" object:nil queue:queue usingBlock:^(NSNotification * _Nonnull note) {
-        NSLog(@"note = %@", note);
-    }];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -47,8 +27,6 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     TXPerson *person = [[TXPerson alloc] init];
     [person test2];
-    
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"notificationTest" object:person];
 }
 
 @end

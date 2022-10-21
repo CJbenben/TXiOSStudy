fc1f5f27d477e1b0644ae639639e855da825709e
chenxiaojie <chenjie003@qq.com>
2022-10-21 17:00:59 +0800

    修改block内存管理

--- 07-block/12-block内存管理/12-block内存管理/main.m
+++ 07-block/12-block内存管理/12-block内存管理/main.m
@@ -24,9 +24,21 @@ int main(int argc, const char * argv[]) {
 //            block = ^{
 //                NSLog(@"person = %@", person);
 //            };
+//            NSLog(@"block -- %@", [block class]);
 //        }
         
+//        {
+//            TXPerson *person = [[TXPerson alloc] init];
+//            __weak TXPerson *weakPerson = person;
+//            block = ^{
+//                NSLog(@"person = %@", weakPerson);
+//            };
+//        }
         
+        /**
+         ARC 环境下：person对象并不会销毁。因为：block内部会强引用 __Block_byref_person_0 对象，
+                    __Block_byref_person_0 也会强引用 person
+         */
 //        {
 //            __block TXPerson *person = [[TXPerson alloc] init];
 //            block = ^{
@@ -34,10 +46,27 @@ int main(int argc, const char * argv[]) {
 //            };
 //        }
         
+        // RMC 环境下：person对象会销毁（即使__NSStackBlock__调用了copy，已经在堆上面了）这个需要注意下
+//        __block TXPerson *person = [[TXPerson alloc] init];
+//        block = [^{
+//            NSLog(@"person = %@", person);
+//        } copy];
+//        NSLog(@"block - %@", [block class]);
+//        [person release];
+        
+
+//        {
+//            __block TXPerson *person = [[TXPerson alloc] init];
+//            __weak TXPerson *weakPerson = person;
+//            block = ^{
+//                NSLog(@"person = %@", weakPerson);
+//            };
+//        }
+        
         
         {
-            __block TXPerson *person = [[TXPerson alloc] init];
-            __weak TXPerson *weakPerson = person;
+            TXPerson *person = [[TXPerson alloc] init];
+            __block __weak TXPerson *weakPerson = person;
             block = ^{
                 NSLog(@"person = %@", weakPerson);
             };
